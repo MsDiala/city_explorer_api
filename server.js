64f8a75302b27e80904436d78ff303953840b168
MsDiala <diala-is-diala@hotmail.com>
2020-10-26 03:10:33 +0300

    6

---
+++ server.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// express library sets up our server
+const express = require('express');
+// initalizes our express library into our variable called app
+const app = express();
+
+// dotenv lets us get our secrets from our .env file
+require('dotenv').config();
+
+// bodyguard of our server - tells who is ok to send data to
+const cors = require('cors');
+app.use(cors());
+
+
+// bring in the PORT by using process.env.variable name
+const PORT = process.env.PORT || 3001;
+
+app.get('/location', (request, response) => {
+  try{
+    console.log(request.query.city);
+    let search_query = request.query.city;
+
+    let geoData = require('./data/location.json');
+
+    let returnObj = new Location(search_query, geoData[0]);
+
+    console.log(returnObj);
+
+    response.status(200).send(returnObj);
+
+  } catch(err){
+    console.log('ERROR', err);
+    response.status(500).send('sorry, we messed up');
+  }
+
+})
+
+function Location(searchQuery, obj){
+  this.search_query = searchQuery;
+  this.formatted_query = obj.display_name;
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
+}
+
+// server
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+})
+
+app.get('/weather', (request, response) => {
+  try{
+
+
+    let weatherData = require('./data/weather.json');
+
+    let weatherArr = [];
+
+    console.log('The weather is', weatherArr);
+    weatherData.data.forEach(weatherLoop => weatherArr.push(new Weather(weatherLoop)));
+
+    response.status(200).send(weatherArr);
+
+  } catch(err){
+    console.log('ERROR', err);
+    response.status(500).send('sorry, we messed up');
+  }
+
+})
+app.get('*', (request, response) => {
+  response.status(404).send('sorry, this route does not exist');
+})
+
+function Weather(obj){
+  this.forecast = obj.weather.description;
+  this.time = obj.valid_date;
+}
